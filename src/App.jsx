eb30004a96b7690df93601cfcdcdae9950ecc228
Micks <Micah.Hack.18@gmail.com>
2025-07-04 21:18:02 +0200

    Added cross svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
-import Filters from './components/Filters/Filters.jsx'
+import './App.css';
+import Filters from './components/Filters/Filters.jsx';
+import JesusCross from './components/JesusCross/JesusCross.jsx';
 
 export default function App() {
   return (
     <>
-        <div className="d-flex flex-column jesusiskingcontainer justify-content-center align-items-center text-center p-1 p-sm-0">
+          <div className="d-flex flex-column jesusiskingcontainer justify-content-center align-items-center text-center p-1 p-sm-0">
+            <JesusCross></JesusCross>
             <h1 className="fw-bold text-white">JESUS IS KING!</h1>
           </div>
         <Filters />
---
+++ src/components/JesusCross/JesusCross.jsx
@@ -0,0 +1,28 @@
+export default function JesusCross() {
+    return (
+        <div className="mb-5">
+        <svg width="100" height="150" viewBox="0 0 100 150">
+            <path
+                d="
+                    M 35 0
+                    V 30
+                    H 5
+                    V 60
+                    H 35
+                    V 150
+                    H 65
+                    V 60
+                    H 95
+                    V 30
+                    H 65
+                    V 0
+                    Z
+                "
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+            />
+            </svg>
+        </div>
+    )
+}
