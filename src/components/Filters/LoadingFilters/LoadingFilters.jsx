673e536b3c0d67f959734db4c558bdb61630639e
Micks <Micah.Hack.18@gmail.com>
2025-07-07 21:35:34 +0200

    Added LoadingFilters component for loading state of Filters component

---
+++ src/components/Filters/LoadingFilters/LoadingFilters.jsx
@@ -0,0 +1,30 @@
+import Shimmer from './../../Shimmer/Shimmer.jsx';
+import ShimmerConfig from './../../../models/ShimmerConfig.js';
+
+export default function LoadingFilters() {
+    const headerShimmer = new ShimmerConfig("50px", "290px", "10");
+    const verseShimmer = new ShimmerConfig("120px", "290px", "10");
+    const buttonShimmer = new ShimmerConfig("37.6px", "41.6", "60");
+
+    return (
+        <>
+            <div className="col-12 col-md-6 d-flex justify-content-center align-items-center mb-5 mb-md-0">
+                <Shimmer></Shimmer>
+            </div>
+            <div className="col-12 col-md-6 d-flex flex-column align-items-center">
+                <Shimmer shimmerConfig={headerShimmer}></Shimmer>
+                <div className="mt-3">
+                    <Shimmer shimmerConfig={verseShimmer}></Shimmer>
+                </div>
+                <div className="d-flex flex-row justify-content-around align-items-center mt-3">
+                    <div className="me-2">
+                        <Shimmer shimmerConfig={buttonShimmer}></Shimmer>
+                    </div>
+                    <div className="ms-2">
+                     <Shimmer shimmerConfig={buttonShimmer}></Shimmer>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
