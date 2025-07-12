8e07254b035fc35ce89b5e3781296f043b45ecd1
Micks <Micah.Hack.18@gmail.com>
2025-07-12 21:12:51 +0200

    Added GithubReferrer component for linking github project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Filters from './components/Filters/Filters.jsx';
 import JesusCross from './components/JesusCross/JesusCross.jsx';
+import GithubReferrer from './components/GithubReferrer/GithubReferrer.jsx';
 
 export default function App() {
   return (
     <>
           <div className="d-flex flex-column jesusiskingcontainer justify-content-center align-items-center text-center p-1 p-sm-0">
-            <JesusCross></JesusCross>
+            <GithubReferrer />
+            <JesusCross />
             <h1 className="fw-bold text-white">JESUS IS KING!</h1>
           </div>
         <Filters />
